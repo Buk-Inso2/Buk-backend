c3e20fb5f0fbe4e8f64ceeae157e6aef05c37bf8
LuisRz1 <langulor1@upao.edu.pe>
2023-12-02 03:50:41 -0500

    first commit

---
+++ src/main/java/edu/pe/upao/buk/BukApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.buk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BukApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pe/upao/buk/BukApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.buk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
